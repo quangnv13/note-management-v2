dfd724d3001208e6eb14dfb776b572bdf6947807
Quang Nguyễn <64973827+quangnv13@users.noreply.github.com>
2021-04-16 17:26:54 +0700

    update

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,127 +1,134 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, CssBaseline, TextField, Link, Box, Typography, Container } from '@material-ui/core';
+import React, { useContext, useState } from "react";
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Box,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { AppContext } from "../context/AppContext";
+import { AUTH } from "../const/actionsConst";
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://quangnguyen.info/">
-                Quang Nguyễn
-        </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://quangnguyen.info/">
+        Quang Nguyễn
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(AppContext);
+  const { appState, appDispatch } = context;
+  const { isValidUsername, isValidPassword } = appState;
+
+  const userNameInput = (event) => {
+    if (event.target.value) {
+      setUsername(event.target.value);
+      appDispatch({
+        type: AUTH.ISVALID_USERNAME,
+        data: { isValid: true },
+      });
+    } else {
+      appDispatch({
+        type: AUTH.ISVALID_USERNAME,
+        data: { isValid: false },
+      });
+    }
+  };
+
+  const passwordInput = (event) => {
+    if (event.target.value) {
+      setPassword(event.target.value);
+      appDispatch({
+        type: AUTH.ISVALID_PASSWORD,
+        data: { isValid: true },
+      });
+    } else {
+      appDispatch({
+        type: AUTH.ISVALID_PASSWORD,
+        data: { isValid: false },
+      });
+    }
+  };
+
+  const btnLoginClick = (event) => {
+    event.preventDefault();
+    appDispatch({
+      type: AUTH.LOGIN,
+      data: {
+        username,
+        password,
+      },
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div>
+        <img
+          className="m-auto d-flex"
+          alt="Sign in icon"
+          height="100px"
+          src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/user-512.png"
+        ></img>
+        <Typography component="h1" variant="h5" className="text-center">
+          Log in
         </Typography>
-    );
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+            error={!isValidUsername}
+            onInput={userNameInput}
+            autoComplete="username"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            autoComplete="current-password"
+            type="password"
+            id="password"
+            error={!isValidPassword}
+            onInput={passwordInput}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={btnLoginClick}
+            disabled={!(isValidUsername && isValidPassword)}
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: '',
-            password: '',
-            inValidUserName: false,
-            inValidPassword: false
-        }
-    }
-
-    userNameInput = (event) => {
-        if (event.target.value) {
-            this.setState({
-                userName: event.target.value,
-                inValidUserName: false
-            });
-        } else {
-            this.setState({
-                inValidUserName: true
-            });
-        }
-    }
-
-    passwordInput = (event) => {
-        if (event.target.value) {
-            this.setState({
-                password: event.target.value,
-                inValidPassword: false
-            });
-        } else {
-            this.setState({
-                inValidPassword: true
-            });
-        }
-    }
-
-    btnLoginClick = (event) => {
-        event.preventDefault();
-        this.props.btnLoginClick(this.state.userName, this.state.password);
-    };
-
-    loginResponse = (isLogged) => {
-        if (isLogged !== undefined && !isLogged) {
-            this.setState({
-                inValidUserName: true,
-                inValidPassword: true
-            });
-        }
-    }
-
-    render() {
-        const { inValidUserName, inValidPassword } = this.state;
-        return (
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div>
-                    <img className="m-auto d-flex" alt="Sign in icon" height="100px" src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/user-512.png"></img>
-                    <Typography component="h1" variant="h5" className="text-center">
-                        Log in
-                    </Typography>
-                    <form noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Username"
-                            name="email"
-                            autoFocus
-                            error={inValidUserName}
-                            onInput={this.userNameInput}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            error={inValidPassword}
-                            onInput={this.passwordInput}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            onClick={this.btnLoginClick}
-                        >
-                            Login
-                        </Button>
-                    </form>
-                </div>
-                <Box mt={8}>
-                    <Copyright />
-                </Box>
-            </Container>
-        );
-    }
-}
-
-Login.propTypes = {
-    btnLoginClick: PropTypes.func.isRequired
-};
-
-Login.defaultProps = {
-};
+export default Login;
