7174b615a1dc759302ab4fd88853a8ec6b39bd2c
Quang Nguyễn <64973827+quangnv13@users.noreply.github.com>
2021-04-16 17:26:16 +0700

    update

---
+++ src/components/const/notifyConst.js
@@ -0,0 +1,11 @@
+export const NOTIFY_TYPE = {
+  SUCCESS: "success",
+  ERROR: "error",
+  WARNING: "warning",
+};
+
+export const NOTIFY_MESSAGE = {
+  ACTION_INVALID: "Action is invalid",
+  LOGIN_SUCCESS: "Login success!",
+  LOGIN_ERROR: "Login error! Please check your credentials!"
+};
