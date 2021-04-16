ca9190a5b67c7dc055879a7454a65da8a5fb759c
Quang Nguyễn <64973827+quangnv13@users.noreply.github.com>
2021-04-16 17:24:40 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React, { useRef, useContext } from "react";
+import React, { useContext } from "react";
 import "./App.css";
 import Login from "./components/login/Login";
 import Note from "./components/notes/Note";
 import { Snackbar } from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
-import { AUTH, CONTROL } from "./actionsConst";
-import { AppContext } from "./AppContext";
+import { CONTROL } from "./components/const/actionsConst";
+import { AppContext } from "./components/context/AppContext";
+import { NoteContextProvider } from "./components/context/NoteContext";
 
 function App() {
-  const context = useContext(AppContext);
-
-  const { appState, appDispatch } = context;
-
+  const { appState, appDispatch } = useContext(AppContext);
   const { isLogged, isShowNotify, notifyMessage, notifyType } = appState;
-
-  const loginComponentRef = useRef();
 
   const Alert = (props) => {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -24,28 +20,10 @@ function App() {
     appDispatch({ type: CONTROL.HIDE_NOTIFY });
   };
 
-  const btnLoginClick = (username, password) => {
-    appDispatch({ type: AUTH.LOGIN, data: { username, password } });
-    // showNotify("Login error! Please check your credentials!", "error");
-    // loginComponentRef.current.loginResponse(false);
-  };
-
-  const btnLogoutClick = () => {
-    appDispatch({ type: AUTH.LOGOUT });
-  };
-
-  const showLoginForm = !isLogged ? (
-    <Login
-      ref={loginComponentRef}
-      btnLoginClick={btnLoginClick}
-      loginResponse={isLogged}
-    ></Login>
-  ) : (
-    <Note btnLogoutClick={btnLogoutClick}></Note>
-  );
+  const showLoginForm = !isLogged ? <Login></Login> : <Note></Note>;
 
   return (
-    <div>
+    <NoteContextProvider>
       {showLoginForm}
       <Snackbar
         open={isShowNotify}
@@ -56,7 +34,7 @@ function App() {
           {notifyMessage}
         </Alert>
       </Snackbar>
-    </div>
+    </NoteContextProvider>
   );
 }
 
